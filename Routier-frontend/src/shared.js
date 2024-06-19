cff5ddc0fd4547ffff59fe853528c2a69967be0f
Het Patel <hetpatel3245@gmail.com>
2024-06-19 19:35:10 +0530

    minor change

--- Routier-frontend/src/shared.js
+++ Routier-frontend/src/shared.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "routier-production.up.railway.app"
-export const baseUrl = "https://routier-production.up.railway.app"
-// export const baseUrl = "http://localhost:8000"
+//export const baseUrl = "https://routier-production.up.railway.app"
+export const baseUrl = "http://localhost:8000"
