8e8268097b9f4cde49ac577d4f06b956629a4b93
Het Patel <hetpatel3245@gmail.com>
2024-07-14 11:18:38 +0530

    deployment issue

--- Routier-frontend/src/shared.js
+++ Routier-frontend/src/shared.js
@@ -1,3 +1,4 @@
 // export const baseUrl = "routier-production.up.railway.app"
 //export const baseUrl = "https://routier-production.up.railway.app"
+//export const baseUrl = "https://routier-jet.vercel.app";
 export const baseUrl = "http://localhost:8000"
